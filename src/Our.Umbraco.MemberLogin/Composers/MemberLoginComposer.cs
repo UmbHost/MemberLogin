5a57acd241685ae7b32d28aa8a7dbea4378fa417
Aaron <aaron@umbhost.net>
2022-11-23 09:17:23 +0000

    Converted to razor class library

---
+++ src/Our.Umbraco.MemberLogin/Composers/MemberLoginComposer.cs
@@ -0,0 +1,15 @@
+﻿using Our.Umbraco.MemberLogin.Core.TreeNotificationHandlers;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.Notifications;
+
+namespace Our.Umbraco.MemberLogin.Core.Composers
+{
+    public class MemberLoginComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            builder.AddNotificationHandler<MenuRenderingNotification, MemberLoginTreeNotificationHandler>();
+        }
+    }
+}
---
+++ src/Our.Umbraco.MemberLogin/TreeNotificationHandlers/MemberLoginTreeNotificationHandler.cs
@@ -0,0 +1,26 @@
+﻿using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Core.Notifications;
+
+namespace Our.Umbraco.MemberLogin.Core.TreeNotificationHandlers
+{
+    public class MemberLoginTreeNotificationHandler : INotificationHandler<MenuRenderingNotification>
+    {
+        public MemberLoginTreeNotificationHandler()
+        {
+        }
+
+        public void Handle(MenuRenderingNotification notification)
+        {
+            // If we are not on the members tree, just return
+            if (notification.TreeAlias != "member") return;
+
+            var menuItem = new global::Umbraco.Cms.Core.Models.Trees.MenuItem("memberLogin", "Impersonate Member");
+            menuItem.AdditionalData.Add("memberId", notification.NodeId);
+            menuItem.AdditionalData.Add("actionView", "/App_Plugins/MemberLogin/views/memberlogin.html");
+            menuItem.Icon = "client";
+            menuItem.SeparatorBefore = false;
+            notification.Menu.Items.Insert(notification.Menu.Items.Count, menuItem);
+
+        }
+    }
+}
