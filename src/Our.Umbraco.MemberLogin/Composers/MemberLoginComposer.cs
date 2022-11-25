d39c3567b5de2a9843f238b2d12fe3768491d4d8
Aaron <aaron@umbhost.net>
2022-11-25 12:19:36 +0000

    Added Manifest filter

--- src/Our.Umbraco.MemberLogin/Composers/MemberLoginComposer.cs
+++ src/Our.Umbraco.MemberLogin/Composers/MemberLoginComposer.cs
@@ -1,4 +1,5 @@
-using Our.Umbraco.MemberLogin.Core.TreeNotificationHandlers;
+﻿using Our.Umbraco.MemberLogin.Core.TreeNotificationHandlers;
+using Our.Umbraco.MemberLogin.ManifestFilters;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
 using Umbraco.Cms.Core.Notifications;
@@ -10,6 +11,7 @@ namespace Our.Umbraco.MemberLogin.Composers
         public void Compose(IUmbracoBuilder builder)
         {
             builder.AddNotificationHandler<MenuRenderingNotification, MemberLoginTreeNotificationHandler>();
+            builder.ManifestFilters().Append<MemberLoginManifestFilter>();
         }
     }
 }
---
+++ src/Our.Umbraco.MemberLogin/ManifestFilters/MemberLoginManifestFilter.cs
@@ -0,0 +1,22 @@
+﻿using Umbraco.Cms.Core.Manifest;
+
+namespace Our.Umbraco.MemberLogin.ManifestFilters;
+
+internal class MemberLoginManifestFilter : IManifestFilter
+{
+    public void Filter(List<PackageManifest> manifests)
+    {
+        manifests.Add(new PackageManifest
+        {
+            PackageName = "MemberLogin",
+            Scripts = new[]
+            {
+                "/App_Plugins/MemberLogin/js/memberlogin.controller.js"
+            },
+            Stylesheets = new[]
+            {
+                "/App_Plugins/MemberLogin/css/memberlogin.css"
+            }
+        });
+    }
+}
